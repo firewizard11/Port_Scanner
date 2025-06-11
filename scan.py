e09a1ec540e9588409f6a73a020fc8ea8e945fe1
Sam <samkaveh@outlook.com>
2025-06-12 09:55:38 +1000

    Wrote is_open() function

--- scan.py
+++ scan.py
@@ -7,7 +7,16 @@ def cli():
 
 
 def is_open(host: str, port: int) -> bool:
-	pass
+	addr = (host, port)
+	
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+		sock.settimeout(1)
+
+		try:
+			sock.connect(addr)
+			return True
+		except:
+			return False
 
 
 def parse_ports(ports: str) -> list[int]:
@@ -15,4 +24,14 @@ def parse_ports(ports: str) -> list[int]:
 
 
 if __name__ == '__main__':
-	cli()
+	print('Starting Test...')
+	
+	test_host = 'scanme.nmap.org'
+	test_ports = [22, 80, 443, 1024, 2000, 3000]
+
+	print('Testing {}'.format(test_host))
+
+	for port in test_ports:
+		print('{}: {}'.format(port, is_open(test_host, port)))
+
+	print('Test Complete!')
