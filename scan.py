76f447c5badb897320c2d47d9ef119a3f08e1f1a
Sam <samkaveh@outlook.com>
2025-06-12 09:34:40 +1000

    Created scan.py

---
+++ scan.py
@@ -0,0 +1,18 @@
+import argparse
+import socket
+
+
+def cli():
+	pass
+
+
+def is_open(host: str, port: int) -> bool:
+	pass
+
+
+def parse_ports(ports: str) -> list[int]:
+	pass
+
+
+if __name__ == '__main__':
+	cli()
