b71b49f2227d51479ecf5bd6ae7181ab0294e591
Sam <samkaveh@outlook.com>
2025-06-12 18:04:28 +1000

    Added Input validation for host and ports

--- scan.py
+++ scan.py
@@ -17,13 +17,27 @@ def cli():
 
     host: str = args.host
 
+    if not validate_ip(host):
+        print('Error: {} is not a valid IP'.format(host))
+        return
+
     try:
         ports: list[int] = parse_ports(args.ports) 
     except ValueError as e:
         print(e)
         return
 
+    for port in ports:
+        if not validate_port(port):
+            print('Error: {} is not a valid port'.format(port))
+            return
+
     threads = args.threads 
+
+    if threads is not None and threads < 1:
+        print('Error: threads must be greater than 0 not {}'.format(threads))
+        return
+
     verbose = args.verbose
 
     print('Testing {}:'.format(host)) 
@@ -85,6 +99,22 @@ def parse_ports(ports: str) -> list[int]:
         return list(range(start, end+1))
 
     raise ValueError('Error: ports {} is not in a valid format (e.g. number, start-end, number,number,...,number)'.format(ports))
+
+
+def validate_port(port: int) -> bool:
+    return 0 <= port <= 65535
+
+
+def validate_ip(ip: str) -> bool:
+    re_ip = re.compile(r'^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$')
+    
+    if re_ip.fullmatch(ip):
+        for octet in ip.split('.'):
+            if not (0 <= int(octet) <= 255):
+                return False
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
